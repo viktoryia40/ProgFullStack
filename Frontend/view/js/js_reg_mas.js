fd632428566a781284ef42637f5576d1eff375b1
Almironlucas <almironlucas2017@gmail.com>
2022-11-04 16:16:15 -0300

    subo validaciones con js en formulario de registro de mascotas

--- Frontend/view/js/js_reg_mas.js
+++ Frontend/view/js/js_reg_mas.js
@@ -2,14 +2,17 @@ function validar(){
     let nombre=document.getElementById("nombre").value;
     if(nombre.length==0 || /^\s+$/.test(nombre)){
         alert("Complete Nombre y Apellido por favor");
+        return false;
     }
     let email=document.getElementById("email").value;
     if(email.length==0 || /^\s+$/.test(email)){
         alert("Complete el email por favor");
+        return false;
     }
     let registro=document.getElementById("registro").value;
     if (registro.length==""){
         alert("Elija una opción por favor")
+        return false;
     }
     let especie=document.getElementById("especie").value;
     let raza=document.getElementById("raza").value;
