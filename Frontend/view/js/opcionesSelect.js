77c89c6049562721519a7837ec2d71d4e61103aa
VanessaVeizaga <vanessaveizaga@gmail.com>
2022-11-07 17:39:48 -0300

    Se agregan selects de provincias y dptos con script correspondiente a registo de mascotas, finalización de compra, y registro de usuario

--- Frontend/view/js/opcionesSelect.js
+++ Frontend/view/js/opcionesSelect.js
@@ -65,23 +65,3 @@ function getRazas() {
     }`
 }
 
-function mostrarFiltro(boton) {
-    boton.className = "btn btn-primary botonOculto"
-    let campos = document.querySelectorAll(".especializado")
-    console.log(campos[0]);
-    console.log(boton.id);
-    if (boton.id == "btnFiltroEspecializado") {
-        document.querySelector(".publicaciones h3").innerHTML = "Filtro especializado"
-        document.querySelector("#btnFiltroRapido").className = "btn btn-primary"
-        for (i = 0; i < campos.length; i++) {
-            campos[i].className = "form-select especializado"
-        }
-    } else {
-        document.querySelector(".publicaciones h3").innerHTML = "Filtro rápido"
-        document.querySelector("#btnFiltroEspecializado").className = "btn btn-primary"
-        for (i = 0; i < campos.length; i++) {
-            campos[i].className = "form-select especializado selectOculto"
-        }
-    }  
-}
-
